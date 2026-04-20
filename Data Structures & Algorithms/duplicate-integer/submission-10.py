44525290e8295ad8b8284f29d6ab369103c91f29
Gaal Jaylaani <79125486+GaalJaylaani@users.noreply.github.com>
2026-04-19 18:46:52 -0700

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        countMap = set()
+        for n in nums:
+            if n in countMap:
+                return True
+            countMap.add(n)
+        return False
